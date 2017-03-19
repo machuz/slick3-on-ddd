7c143e2e2c4564501d24cf5323775b030111081b
matsukawa_tsubasa <w_ma2k8@me.com>
2017-03-19 21:07:01 +0900

    add controller

---
+++ app/adapter/input/controller/user/UserController.scala
@@ -0,0 +1,81 @@
+package adapter.input.controller.user
+
+import com.google.inject.Inject
+
+import adapter.input.json.model.user.{ UserCreateRequest, UserDeleteRequest, UserUpdateRequest }
+import slick.dbio.DBIO
+
+import scala.concurrent.Future
+
+import scalaz.{ -\/, \/- }
+
+import play.api.mvc.{ Action, Controller, Result }
+
+import domain.model.user.UserId
+import domainInterface.adapter.output.{ CreateUserResultPresenter, DeleteUserResultPresenter, ListUserResultPresenter, UpdateUserResultPresenter }
+import domainInterface.usecase.user.{ CreateUserUseCase, DeleteUserUseCase, ListUserUseCase, UpdateUserUseCase }
+
+class UserController @Inject()(
+  createUserUseCase: CreateUserUseCase[DBIO],
+  updateUserUseCase: UpdateUserUseCase[DBIO],
+  deleteUserUseCase: DeleteUserUseCase,
+  listUserUseCase: ListUserUseCase[DBIO],
+  createUserResultPresenter: CreateUserResultPresenter[Result],
+  updateUserResultPresenter: UpdateUserResultPresenter[Result],
+  deleteUserResultPresenter: DeleteUserResultPresenter[Result],
+  listUserResultPresenter: ListUserResultPresenter[Result]
+) extends Controller {
+  protected[this] implicit def defaultContext = play.api.libs.concurrent.Execution.defaultContext
+
+  def create = Action.async { implicit req =>
+    req.body.asJson.map(UserCreateRequest.fromJson) match {
+      case Some(\/-(x)) =>
+        val uc = createUserUseCase.execute(x.name)
+        createUserResultPresenter.response(uc) recover {
+          case e: Throwable => BadRequest(e.getMessage)
+        }
+      case Some(-\/(e)) =>
+        Future.successful { BadRequest(s"""parse failed: [${e.map(_._1).mkString(",")}]""") }
+      case _ =>
+        Future.successful { BadRequest(s"""request param is not json""") }
+    }
+  }
+
+  def update = Action.async { implicit req =>
+    req.body.asJson.map(UserUpdateRequest.fromJson) match {
+      case Some(\/-(x)) =>
+        val args = (UserId(x.userId), x.name)
+        val uc = updateUserUseCase.execute(args)
+        updateUserResultPresenter.response(uc) recover {
+          case e: Throwable => BadRequest(e.getMessage)
+        }
+      case Some(-\/(e)) =>
+        Future.successful { BadRequest(s"""parse failed: [${e.map(_._1).mkString(",")}]""") }
+      case _ =>
+        Future.successful { BadRequest(s"""request param is not json""") }
+    }
+  }
+
+  def delete = Action.async { implicit req =>
+    req.body.asJson.map(UserDeleteRequest.fromJson) match {
+      case Some(\/-(x)) =>
+        val args = UserId(x.userId)
+        val uc = deleteUserUseCase.execute(args)
+        deleteUserResultPresenter.response(uc) recover {
+          case e: Throwable => BadRequest(e.getMessage)
+        }
+      case Some(-\/(e)) =>
+        Future.successful { BadRequest(s"""parse failed: [${e.map(_._1).mkString(",")}]""") }
+      case _ =>
+        Future.successful { BadRequest(s"""request param is not json""") }
+    }
+  }
+
+  def list = Action.async { implicit req =>
+    val res = listUserUseCase.execute(())
+    listUserResultPresenter.response(res) recover {
+      case e: Throwable => BadRequest(e.getMessage)
+    }
+  }
+
+}
